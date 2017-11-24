950758c7c0648304541cbfb9b3c4e8948c9adc93
Christine Wasike <christinewasike22@gmail.com>
2017-11-24 11:23:08 +0300

    Added an attribute to the p element in my rendered template, it has the attribute data-myAttribute

--- App.jsx
+++ App.jsx
@@ -1,1 +1,15 @@
- 
+ import React from 'react';
+
+ class App extends React.Component {
+     render() {
+         return (
+             <div>
+                 <h1>Welcome to the world of coding</h1>
+                 <h2>A whole new world</h2>
+                 <p data-yAttribute = "somevalue">I can show you the world, take you wonder by wonder. Over mountains and under on a magic carpet ride. </p>
+                 </div>
+         );
+     }
+ }
+
+ export default App;
