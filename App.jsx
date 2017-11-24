c3d308080ba7c5c7e0f2b123602f91f59941a59e
Christine Wasike <christinewasike22@gmail.com>
2017-11-24 11:38:17 +0300

    Changed the attribute in the p tag

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@
              <div>
                  <h1>Welcome to the world of coding</h1>
                  <h2>A whole new world</h2>
-                 <p data-yAttribute = "somevalue">I can show you the world, take you wonder by wonder. Over mountains and under on a magic carpet ride. </p>
+                 <p data-myAttribute = "somevalue">I can show you the world, take you wonder by wonder. Over mountains and under on a magic carpet ride. </p>
                  </div>
          );
      }
