a6f364825ea5cd1e9ad71ffed594be9ba00e0841
Christine Wasike <christinewasike22@gmail.com>
2017-12-08 08:27:29 +0300

    Worked on the FindDom Node concept

--- App.jsx
+++ App.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
     constructor() {
         super();
-
-        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+        this.FindDomNodeHandler = this.FindDomNodeHandler.bind(this);
     };
-    forceUpdateHandler() {
-        this.forceUpdate();
-    };
+    FindDomNodeHandler() {
+        var myDiv = document.getElementById('myDiv');
+        ReactDOM.findDOMNode(myDiv).style.color = 'green';
+    }
     render() {
         return (
             <div>
-                <button onClick={this.forceUpdateHandler}>FORCE UPDATE</button>
-                <h4>Random number: {Math.random()}</h4>
+                <button onClick = {this.FindDomNodeHandler}>FIND DOME NODE</button>
+                <div id="myDiv">NODE</div>
             </div>
-        );
+        )
     }
 }
-
 export default App;
